ab9fe707e1ada40600b3812cf25814e4ceae1eef
Darko Mijatovic <dmijato1@gmail.com>
2017-06-26 22:02:32 -0400

    Included functionality to: 1. Check to make sure the user submitted letter is in fact a letter. 2. Check that the user does not guess the same letter more than once. 3. Matches the guessed letter to the word and checks to see if the user won, lost, or can continues playing.

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,15 +1,69 @@
 class HangpersonGame
 
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
+  attr_accessor :word, :guesses, :wrong_guesses, :word_with_guesses
 
   # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
-  
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+    @word_with_guesses = ""
+    1.upto(word.split("").count) { @word_with_guesses += "-" }
+    @attempts = 1
+  end
+
+  # Function gets a letter from the user and checks if its a correct guess.
+  # The user must guess the word within 7 tries or else he/she will lose
+  def guess(letter)
+    # STEP 1: check to make sure the letter is not any non-letter character and
+    # raises an error if it is
+    if /^[a-zA-Z]+$/.match(letter)
+      letter.downcase!
+    else raise_error(ArgumentError)
+    end
+
+    # STEP 2: check to make sure the user does not enter the same letter more
+    # than once, and if so, returns false
+    if @guesses.include? letter; return false; end
+    if @wrong_guesses.include? letter; return false; end
+
+    # STEP 3: check to see if the guessed letter matches any letters in the 
+    # word
+    if @word.include? letter
+        @guesses += letter
+        # if there is a match, update the word_with_guesses to show where
+        # in the string the letter was matched
+        @word.each_char.with_index do |c, i|
+          if c == letter
+            @word_with_guesses[i] = c
+          end
+        end 
+    else @wrong_guesses += letter
+    end
+    # update the number of attempts and call the function to check if
+    # the user won, lost, or continues playing
+    @attempts += 1
+    check_win_or_lose
+  end
+
+  # function for dealing with multiple guesses at once
+  def guess_several_letters(object, string)
+    string.each { |letter| guess(letter) }
+  end
+
+  # function for checking if user won, lost, or continues playing
+  def check_win_or_lose
+    if !@word_with_guesses.include?("-") && @attempts < 8
+      return :win
+    elsif @word_with_guesses.include?("-") && @attempts == 8
+      return :lose
+    else 
+      return :play 
+    end
+  end
+
+  def raise_error(e)
+    raise ArgumentError.new(e)
   end
 
   def self.get_random_word
